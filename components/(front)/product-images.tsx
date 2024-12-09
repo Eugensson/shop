0b420307ce2d9835f99c4b54316971d9edce41d8
Serhii Kulbashnyi <112683442+Eugensson@users.noreply.github.com>
2024-12-09 21:33:48 +0200

    Updated Product Image component

--- components/(front)/product-images.tsx
+++ components/(front)/product-images.tsx
@@ -2,8 +2,11 @@
 
 import Image from "next/image";
 import { useState } from "react";
+import Zoom from "react-medium-image-zoom";
 
 import { cn } from "@/lib/utils";
+
+import "react-medium-image-zoom/dist/styles.css";
 
 interface ProductImagesProps {
   className?: string;
@@ -15,29 +18,39 @@ export const ProductImages = ({ images, className }: ProductImagesProps) => {
 
   return (
     <div className={cn("space-y-4", className)}>
-      <Image
-        src={images[current] ?? "/images/placeholder.png"}
-        alt="Product image"
-        width={1000}
-        height={1000}
-        className="min-h-[300px] object-cover object-center aspect-square"
-      />
+      <Zoom classDialog="custom-zoom">
+        <Image
+          src={images[current] ?? "/images/placeholder.png"}
+          alt="Product image"
+          width={1000}
+          height={1000}
+          className="min-h-[300px] object-cover object-center aspect-square"
+        />
+      </Zoom>
       <ul className="flex items-center gap-x-2">
         {images.map((image, index) => (
           <li
             key={image}
             className={cn(
-              "border-2 mr-2 cursor-pointer hover:border-primary",
+              "relative border-2 mr-2 cursor-pointer hover:border-primary",
               current === index && "border-primary"
             )}
+            role="option"
+            aria-selected={current === index}
             onClick={() => setCurrent(index)}
           >
             <Image
-              src={image}
+              src={image.trim() ? image.trim() : "/placeholder.png"}
               width={95}
               height={95}
               alt="Product image"
               className="object-cover object-center aspect-square"
+            />
+            <div
+              className={cn(
+                "absolute top-0 left-0 w-full h-full bg-black/65 group-hover:bg-transparent",
+                current === index && "bg-transparent"
+              )}
             />
           </li>
         ))}
